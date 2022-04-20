578a5cb6c34c18faf033c8198ba7c0ee9bcf0121
Jorge Vargas <eduardo.borisq@gmail.com>
2022-04-19 22:58:48 -0500

    Requerimiento 2: modificar dateCreated y lastUpdated a tipo fecha

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -4,7 +4,7 @@ describe("Unit Tests for User class", () => {
 
     test('Create an User object', () => {
         //Aqui invocas el codigo que vas a usar en tu app
-        const user = new User(1, "jorgeboris", "Jorge", "Bio", "dateCreated", "lastUpdated")
+        const user = new User(1, "jorgeboris", "Jorge", "Bio")
 
         //Aqui validas los resultados de ese código
         //Esta es una comparación que va a igualar el valor de la izquierda con el valor de la derecha (valor esperado)
@@ -13,8 +13,7 @@ describe("Unit Tests for User class", () => {
         expect(user.username).toBe("jorgeboris")
         expect(user.name).toBe("Jorge")
         expect(user.bio).toBe("Bio")
-        expect(user.dateCreated).toBe("dateCreated")
-        expect(user.lastUpdated).toBe("lastUpdated")
-
+        expect(user.dateCreated).not.toBeUndefined()
+        expect(user.lastUpdated).not.toBeUndefined()
     });
 })
