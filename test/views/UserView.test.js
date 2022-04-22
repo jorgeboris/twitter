d8deeb7d82679f07750de57ba4f9e279e2a71890
Jorge Vargas <eduardo.borisq@gmail.com>
2022-04-22 00:25:05 -0500

    Views - Requerimiento 2: Validar que el payload regrese un error al tener el valor null en una llave

--- test/views/UserView.test.js
+++ test/views/UserView.test.js
@@ -5,6 +5,14 @@ describe ("Tests for UserView", () => {
     test("Return an error object when try to create a new user with an null payload", () => {
         const payload = null
         const result = UserView.createUser(payload)
-        expect (result.error).toMatch(/payload no exist/)
+        expect(result.error).toMatch(/payload no existe/)
     });
+
+    test("Return an error object when try to create a new user with a payload with invalid properties", () => {
+        const payload = {username: null, name: 12, id: "id"}
+        const result = UserView.createUser(payload)
+        expect(result.error).toMatch(/necesitan tener un valor valido/)
+    });
+
+    
 })
