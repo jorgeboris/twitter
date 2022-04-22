1e1e9f24119669aaa6edacba990f6a24a5d1f21a
Jorge Vargas <eduardo.borisq@gmail.com>
2022-04-22 00:26:10 -0500

    Views - Requerimiento 3: Validar que el payload regrese un error en username

--- test/views/UserView.test.js
+++ test/views/UserView.test.js
@@ -14,5 +14,9 @@ describe ("Tests for UserView", () => {
         expect(result.error).toMatch(/necesitan tener un valor valido/)
     });
 
-    
+    test("Return an error object when try to create a new user with a payload with missing properties", () => {
+        const payload = {username: "Username"}
+        const result = UserView.createUser(payload)
+        expect(result.error).toMatch(/necesitan tener un valor valido/)
+    });
 })
